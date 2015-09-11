360453f899e9ed9be359079fe0605065eff7ec15
beyondvalence <spacewolf13@gmail.com>
2015-09-11 13:57:32 -0400

    more edits

--- script1.R
+++ script1.R
@@ -11,3 +11,18 @@ table$country <- as.factor(table$country)
 table$continent <- as.factor(table$continent)
 str(table)
 summary(table)
+
+write.csv(table, "gapminder_gdp.csv", quote=F)
+
+reg <- lm(data=table, lifeExp ~ year)
+summary(reg)
+plot(table$year, table$lifeExp)
+
+# make data.frame example
+data <- data.frame(c("Wayne"), c("Liu"), c(26))
+names(data) <- c("firstname", "lastname", "age")
+data
+new_person <- data.frame(c("Newton"), c("Vidal"), c(34))
+names(new_person) <- c("firstname", "lastname", "age")
+data2 <- rbind(data,new_person)
+data2
